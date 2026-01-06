67d06837bd0113b6d79b3c1c09ba8b2a14ed38aa
DylanW007 <453480@bsd48.org>
2026-01-05 18:40:52 -0800

    Refactor Node class and include Node.h
    Refactor Node class to use header file and remove unnecessary includes.

--- main.cpp
+++ main.cpp
@@ -1,42 +1,4 @@
-#include <iostream>
-#include <cstring>
-
-using namespace std;
-
-class Node
-{
-public:
-    Node() {
-        this->next = nullptr;
-        strcpy(name, "empty");
-        cout << "Node() - constructor." << endl;
-        
-    }
-    Node(char* str) {
-        strcpy(name, str);
-        cout << "Node(" << str << ") - constructor." << endl;
-    }
-    
-    void setNext(Node* next) {
-        this->next = next;
-    }
-    
-    Node* getNext() {
-        return this->next;
-    }
-    
-    char* getStudent() { return this->name; }
-    
-    ~Node() { cout << "~Node(" << name << ") - destructor." << endl; }
-    
-    void print() {
-        cout << "Node::print(" << this->name << ")" << endl;
-    }
-private:
-    Node* next;
-    
-    char name[255];
-};
+#include "Node.h"
 
 int main()
 {
