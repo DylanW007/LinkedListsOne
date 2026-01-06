3b8c18c69afafb6229c4bb07c90703f2f8ab6a73
DylanW007 <453480@bsd48.org>
2026-01-05 18:39:50 -0800

    Implement Node class with constructors and methods

--- Node.cpp
+++ Node.cpp
@@ -1,1 +1,25 @@
+#include "Node.h"
 
+Node::Node() {
+    this->next = nullptr;
+    strcpy(name, "empty");
+    cout << "Node() - constructor." << endl;
+    
+}
+
+Node::Node(char* str) {
+    strcpy(name, str);
+    cout << "Node(" << str << ") - constructor." << endl;
+}
+
+void Node::setNext(Node* next) {
+    this->next = next;
+}
+
+Node* Node::getNext() {
+    return this->next;
+}
+
+void Node::print() {
+    cout << "Node::print(" << this->name << ")" << endl;
+}
