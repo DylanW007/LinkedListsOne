47934e9d088c8d6ba29a9fc65d9e3d8cc7e0b65b
DylanW007 <453480@bsd48.org>
2026-01-06 08:40:26 -0800

    Replace Node instances with Student instances

--- main.cpp
+++ main.cpp
@@ -1,21 +1,33 @@
 #include "Node.h"
+#include "Student.h"
 
 int main()
 {
-    Node* a = new Node("a");
-    Node* b = new Node("b");
-    Node* c = new Node("c");
-    Node* d = new Node("d");
+    Student* a = new Student(100, "Bobby");
+    Student* b = new Student(101, "Susan");
+    Student* c = new Student(102, "Greg");
+    Student* d = new Student(103, "Brayden");
     
-    a->setNext(b);
-    b->setNext(c);
-    c->setNext(d);
     
-    Node* cur = a;
+    Node* na = new Node(a);
+    Node* nb = new Node(b);
+    Node* nc = new Node(c);
+    Node* nd = new Node(d);
+    
+    na->setNext(nb);
+    nb->setNext(nc);
+    nc->setNext(nd);
+    
+    Node* cur = na;
     while(cur != nullptr) {
         cur->print();
         cur = cur->getNext();
     }
+    
+    delete na;
+    delete nb;
+    delete nc;
+    delete nd;
     
     delete a;
     delete b;
