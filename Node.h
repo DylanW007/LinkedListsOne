e1f53b0db67adbd2ff1ac6a7857a632399e544f3
DylanW007 <453480@bsd48.org>
2026-01-06 08:35:57 -0800

    Adding comments to Node.h
    Updated Node class to support linked lists for Student objects.

--- Node.h
+++ Node.h
@@ -1,29 +1,46 @@
+/*
+Dylan Waters
+Last updated 1/6/2025
+
+Name : Linked Lists 1
+
+Description : Adding node to support linked lists for students.
+*/
 #include <iostream>
 #include <cstring>
+#include "Student.h"
 
 using namespace std;
 
 #ifndef NODE_H
 #define NODE_H
 
+// class defintion for Node to support linked lists for students
 class Node
 {
 public:
+    // Constructors
     Node();
-    Node(char* str);
+    Node(Student* student);
 
-   ~Node() { cout << "~Node(" << name << ") - destructor." << endl; }
-    
+    // Destructor
+    ~Node() {}
+
+    // methods for adding and getting nodes for linked list
     void setNext(Node* next);
     Node* getNext();
-    
-    char* getStudent() { return this->name; }
 
+    // Methods to set and get student data associated with node
+    Student* getStudent() { return this->student; }
+    void  setStudent(Student* student) { this->student = student; }
+
+    // method to print contents of node
     void print();
 private:
+    // Next pointer for linked list
     Node* next;
     
-    char name[255];
+    Student* student;
 };
 
 #endif
