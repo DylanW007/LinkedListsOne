d76b16c550a00d527a14b95df19e18b49fa8550a
DylanW007 <453480@bsd48.org>
2026-01-06 08:38:53 -0800

    Update comments for Node.cpp
    Updated Node class to use Student pointer and removed name handling.

--- Node.cpp
+++ Node.cpp
@@ -1,25 +1,39 @@
+/*
+Dylan Waters
+Last updated 1/6/2025
+
+Name : Linked Lists 1
+
+Description : Node implementation to support linked lists for students.
+*/
 #include "Node.h"
 
+// Default constructor
 Node::Node() {
     this->next = nullptr;
-    strcpy(name, "empty");
-    cout << "Node() - constructor." << endl;
-    
+    student = nullptr;
 }
 
-Node::Node(char* str) {
-    strcpy(name, str);
-    cout << "Node(" << str << ") - constructor." << endl;
+// Constructor that takes student pointer
+Node::Node(Student* student) {
+    this->student = student;
 }
 
+// Linked list set next pointer
 void Node::setNext(Node* next) {
     this->next = next;
 }
 
+// Linked list get next pointer
 Node* Node::getNext() {
     return this->next;
 }
 
+// Print node contents which calls student print
 void Node::print() {
-    cout << "Node::print(" << this->name << ")" << endl;
+    if (this->student != nullptr) {
+        this->student->print();
+    } else {
+        cout << "Empty node" << endl;
+    }
 }
