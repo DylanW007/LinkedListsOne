554871908d7c8b79e9a61f6b69896bbfb7dcd7f7
DylanW007 <453480@bsd48.org>
2026-01-05 18:20:34 -0800

    Define Node class with constructor and destructor

--- Node.h
+++ Node.h
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <cstring>
 
+using namespace std;
+
+class Node
+{
+public:
+    Node();
+    Node(char* str);
+
+   ~Node() { cout << "~Node(" << name << ") - destructor." << endl; }
+    
+    void setNext(Node* next);
+    Node* getNext();
+    
+    char* getStudent() { return this->name; }
+
+    void print();
+private:
+    Node* next;
+    
+    char name[255];
+};
